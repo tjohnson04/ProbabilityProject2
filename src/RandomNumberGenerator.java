ff7c893892e08b9a03796a7054576c0712c6db41
Thomas Johnson <qpq7th@virginia.edu>
2023-03-20 16:36:58 -0400

    added random number generator

---
+++ src/RandomNumberGenerator.java
@@ -0,0 +1,24 @@
+public class RandomNumberGenerator {
+    public static void main (String[] args){
+        int modulus = (int) Math.pow(2, 17);
+        System.out.println(%modulus);
+    }
+    public double calculateU() {
+        double result;
+        int seed = 1000;
+        int multiplier = 24693;
+        int increment = 3517;
+        int modulus = (int) Math.pow(2, 17);
+        int x=0;
+
+        for (int i = 0; i < 500; i ){
+            x=
+        }
+
+    }
+
+    public double rand (int x){
+        return ((seed*x)+increment)/modulus
+    }
+
+}
