84047285f608b4a0da1d1d212958d5548692ac6e
JacobPlummer02 <123589437+JacobPlummer02@users.noreply.github.com>
2023-03-20 18:36:05 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static int count;
+    public static void main (String[] args){
+        double x=0;
+        RandomNumberGenerator r1= new RandomNumberGenerator();
+        double total=0;
+        for (int i=0; i<500; i++){
+            x= r1.findW();
+            System.out.println(i+1+": "+x);
+            total+=x;
+        }
     }
 }
